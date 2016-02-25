e1cf426ecf25f82a2c255e9dbcb724a39b078f35
Federico Razzoli <federico_raz@yahoo.it>
2016-02-25 07:49:03 +0100

    defrag.sql, compress.sql

---
+++ compress.sql
@@ -0,0 +1,54 @@
+/*
+        This file contains the examples used to demonstrate InnoDB Page Compression.
+*/
+
+
+USE test;
+
+
+/*
+        Populate an uncompressed table and check size
+*/
+
+SET GLOBAL innodb_file_per_table := 1;
+CREATE /*M!100000 OR REPLACE */ TABLE t_uncompressed (
+        a INT AUTO_INCREMENT PRIMARY KEY,
+        b BIGINT,
+        c BIGINT,
+        INDEX idx_b (b),
+        INDEX idx_c (c)
+)
+        ENGINE = InnoDB;
+
+INSERT INTO t_uncompressed (a, b, c)
+        SELECT NULL, TRUNCATE(RAND() * 2000, 0), TRUNCATE(RAND() * 100, 0)
+        FROM seq_1_to_1000000;
+
+\! ls -l /var/lib/mysql/test/t_uncompressed.ibd | awk '{ print $5 }'
+
+
+
+/*
+        Populate a compressed table and check size
+*/
+
+SET GLOBAL innodb_compression_algorithm = 'zlib';
+SET GLOBAL innodb_compression_level = 6;
+
+CREATE /*M!100000 OR REPLACE */ TABLE t_compressed (
+        a INT AUTO_INCREMENT PRIMARY KEY,
+        b BIGINT,
+        c BIGINT,
+        INDEX idx_b (b),
+        INDEX idx_c (c)
+)
+        ENGINE = InnoDB
+        PAGE_COMPRESSED = 1;
+
+INSERT INTO t_compressed (a, b, c)
+        SELECT NULL, TRUNCATE(RAND() * 2000, 0), TRUNCATE(RAND() * 100, 0)
+        FROM seq_1_to_1000000;
+
+\! ls -l /var/lib/mysql/test/t_compressed.ibd | awk '{ print $5 }'
+
+
